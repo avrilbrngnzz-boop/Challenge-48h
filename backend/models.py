5ae4cdfd9ace423a9ea84647e0a4a9585b2cbb56
Darwiks <david.raoux@ynov.com>
2026-03-31 11:37:58 +0200

    database.py et models.py

---
+++ backend/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, Float, String
+from database import Base
+
+class Mesure(Base):
+    __tablename__ = "mesures"
+
+    id = Column(Integer, primary_key=True, index=True)
+    station_id = Column(String)
+    station_name = Column(String)
+    lat = Column(Float)
+    lon = Column(Float)
+    indice = Column(Float)
+    horaire = Column(String)
+    date = Column(String)
